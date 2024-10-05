566369c827e25afdde12499da5846e1248df4bff
Angelo <angelowhite871@gmail.com>
2024-10-04 20:35:44 -0400

    .

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 import './style.css'
 import Phaser from 'phaser'
 
-
-
-const config = {
-  type: Phaser.WEBGL,
-  width:500,
-  height:500,
-  
-
+window.onload = function() {
+  var game = new Phaser.game();
 }
